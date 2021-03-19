0b2396ddf9daa878b2177d5deb582613d80ad982
ambergupta1199 <79975210+ambergupta1199@users.noreply.github.com>
2021-03-19 12:44:21 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
